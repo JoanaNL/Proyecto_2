851e17f76b68885662048e3d41363b9074e9d50e
JoanaNL <joananegreiro91@outlook.es>
2025-06-15 18:30:32 +0200

    Actualización

--- api_modelo/api_modelo.py
+++ api_modelo/api_modelo.py
@@ -1,9 +1,11 @@
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import numpy as np
 import os
+import uvicorn
+import sklearn
+
 
 # Carregar el model entrenat
 model_path = os.path.join(os.path.dirname(__file__), "modelo_entrenado_v170.pkl")
@@ -20,9 +22,16 @@ class Cliente(BaseModel):
 
 # Definir l'endpoint
 @app.post("/predecir")
+def predecir_endpoint(cliente: Cliente):
+    return predecir(cliente)
+
 def predecir(cliente: Cliente):
     # Convertir les dades a array per a predicció
     datos = np.array([[cliente.age, cliente.balance, cliente.campaign]])
     prob = model.predict_proba(datos)[0][1]
     return {"probabilidad_si": prob}
+
+
+import api_modelo
+print(api_modelo.app)
 
